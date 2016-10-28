3d77a39996217a550cdf2b96e2fac30c21982cce
nadjieh <ajafari@cern.ch>
2016-10-28 16:46:57 +0200

    Skimmer to include a boson and higgs with corrected bs

--- macro/Skimmer.C
+++ macro/Skimmer.C
@@ -51,6 +51,7 @@ int main(int argc, char** argv) {
     TString names;
     TString treename = "Hamb/Trees/Events";
     TH1D * cutflow = 0;
+    bool forOpt = true;
     bool blind = false;
     for (int f = 1; f < argc; f++) {
         std::string arg_fth(*(argv + f));
@@ -62,6 +63,11 @@ int main(int argc, char** argv) {
             int pos = out.find(".root");
             names = (out.substr(0, pos).c_str());
 	    cutflow = new TH1D(*(TH1D*) fdata->Get(string("Hamb/CutFlowTable/CutFlowTable_").c_str()+names+string("_0").c_str()));
+        } else if(arg_fth == "forOpt"){
+            f++;
+            std::string out(*(argv + f));
+            if(!(out == "True" || out == "true" || out == "yes" || out == "Yes"))
+            	forOpt = false;       
         }
     }
 
@@ -73,46 +79,52 @@ int main(int argc, char** argv) {
 	
 	particleinfo mH, mHReg, mHb, mHbReg;
         TTree * newTree = rds->fChain->CloneTree(0);
-        newTree->Branch("mH", &mH, "pt:eta:phi:mass:b1Index:b2Index");
-        newTree->Branch("mHb", &mHb, "pt:eta:phi:mass:b1Index:b2Index");
-        newTree->Branch("mHReg", &mHReg, "pt:eta:phi:mass:b1Index:b2Index");
-        newTree->Branch("mHbReg", &mHbReg, "pt:eta:phi:mass:b1Index:b2Index");
-
+        if(forOpt){
+	        newTree->Branch("mH", &mH, "pt:eta:phi:mass:b1Index:b2Index");
+    	    newTree->Branch("mHb", &mHb, "pt:eta:phi:mass:b1Index:b2Index");
+    	    newTree->Branch("mHReg", &mHReg, "pt:eta:phi:mass:b1Index:b2Index");
+    	    newTree->Branch("mHbReg", &mHbReg, "pt:eta:phi:mass:b1Index:b2Index");
+		} else {
+	        newTree->Branch("higgs", &mH, "pt:eta:phi:mass:b1Index:b2Index");
+    	    newTree->Branch("abjet", &mHb, "pt:eta:phi:mass:b1Index:b2Index");
+    	    newTree->Branch("higgsReg", &mHReg, "pt:eta:phi:mass:b1Index:b2Index");
+    	    newTree->Branch("abjetReg", &mHbReg, "pt:eta:phi:mass:b1Index:b2Index");		
+		}
         cout << rds->fChain->GetEntriesFast() << endl;
         for (int eventNumber = 0; eventNumber < rds->fChain->GetEntriesFast(); eventNumber++) {
 	    particleinfo tmp;
 	    mH = mHReg = mHb = mHbReg = tmp;
             rds->GetEntry(eventNumber);
-
-	    if (rds->muPt->size() <  2)  continue;
-            if (rds->jetsPt->size() <  2) continue;
-            if (rds->muPt->at(0) < 25 ) continue;
-            if (rds->muPt->at(1) < 10 )  continue;
-            if (rds->jetsPt->at(0) < 20 ) continue;
-            if (rds->jetsPt->at(1) < 15 )  continue;
-            if (rds->metSig > 2 )  continue;
-            TLorentzVector m1,m2,a;
-            m1.SetPtEtaPhiM(rds->muPt->at(0),
-                   rds->muEta->at(0),
-                   rds->muPhi->at(0),0);
-            m2.SetPtEtaPhiM(rds->muPt->at(1),
-                   rds->muEta->at(1),
-                   rds->muPhi->at(1),0);                   
-            a = m1+m2;       
-            if (a.M() < 15 || a.M() > 70) continue;
-	    //No b-flavor requested
-	    TLorentzVector bJet1,bJet2, H;
-            bJet1.SetPtEtaPhiE(rds->jetsPt->at(0), rds->jetsEta->at(0), rds->jetsPhi->at(0), rds->jetsE->at(0));
-            bJet2.SetPtEtaPhiE(rds->jetsPt->at(1), rds->jetsEta->at(1), rds->jetsPhi->at(1), rds->jetsE->at(1));
-	    H = bJet1+bJet2+a;
-	    mH.set(H.Pt(), H.Eta(), H.Phi(), H.M(), 0,1);
-	    double R = a.M()/(bJet1+bJet2).M();
-	    bJet1 = R*bJet1;
-	    bJet2 = R*bJet2;
-	    H = bJet1+bJet2+a;
-	    mHReg.set(H.Pt(), H.Eta(), H.Phi(), H.M(), 0,1);
-
-	    //With b-flavor
+		if(forOpt){
+		    if (rds->muPt->size() <  2)  continue;
+    	        if (rds->jetsPt->size() <  2) continue;
+    	        if (rds->muPt->at(0) < 25 ) continue;
+    	        if (rds->muPt->at(1) < 10 )  continue;
+    	        if (rds->jetsPt->at(0) < 20 ) continue;
+    	        if (rds->jetsPt->at(1) < 15 )  continue;
+    	        if (rds->metSig > 2 )  continue;
+    	        TLorentzVector m1,m2,a;
+    	        m1.SetPtEtaPhiM(rds->muPt->at(0),
+    	               rds->muEta->at(0),
+    	               rds->muPhi->at(0),0);
+    	        m2.SetPtEtaPhiM(rds->muPt->at(1),
+    	               rds->muEta->at(1),
+    	               rds->muPhi->at(1),0);                   
+    	        a = m1+m2;       
+    	        if (a.M() < 15 || a.M() > 70) continue;
+		    //No b-flavor requested
+		    TLorentzVector bJet1,bJet2, H;
+    	        bJet1.SetPtEtaPhiE(rds->jetsPt->at(0), rds->jetsEta->at(0), rds->jetsPhi->at(0), rds->jetsE->at(0));
+    	        bJet2.SetPtEtaPhiE(rds->jetsPt->at(1), rds->jetsEta->at(1), rds->jetsPhi->at(1), rds->jetsE->at(1));
+		    H = bJet1+bJet2+a;
+		    mH.set(H.Pt(), H.Eta(), H.Phi(), H.M(), 0,1);
+		    double R = a.M()/(bJet1+bJet2).M();
+		    bJet1 = R*bJet1;
+		    bJet2 = R*bJet2;
+			    H = bJet1+bJet2+a;
+		    mHReg.set(H.Pt(), H.Eta(), H.Phi(), H.M(), 0,1);
+	
+		    //With b-flavor
             std::vector<int> bQuarkJetIndecies;
             std::vector<TLorentzVector> bQuarkJets;
             for(unsigned int iJet = 0; iJet < rds->jetsPt->size(); iJet++){
@@ -121,25 +133,51 @@ int main(int argc, char** argv) {
             		TLorentzVector b;
             		b.SetPtEtaPhiE(rds->jetsPt->at(iJet), rds->jetsEta->at(iJet), rds->jetsPhi->at(iJet), rds->jetsE->at(iJet));
             		bQuarkJets.push_back(b);	
-        	}
+        		}
     	    }
            if(bQuarkJets.size() > 1 ) {
-	   	H = bQuarkJets[0]+bQuarkJets[1]+a;
-	    	mHb.set(H.Pt(), H.Eta(), H.Phi(), H.M(), bQuarkJetIndecies[0],bQuarkJetIndecies[1]);
-	    	R = a.M()/(bQuarkJets[0]+bQuarkJets[1]).M();
-	    	bQuarkJets[0] = R*bQuarkJets[0];
-	    	bQuarkJets[1] = R*bQuarkJets[1];
-	    	H = bQuarkJets[0]+bQuarkJets[1]+a;
-	    	mHbReg.set(H.Pt(), H.Eta(), H.Phi(), H.M(), 0,1);
-	    }
-	    newTree->Fill();
-        }
-        newTree->Write();
+			   	H = bQuarkJets[0]+bQuarkJets[1]+a;
+		    	mHb.set(H.Pt(), H.Eta(), H.Phi(), H.M(), bQuarkJetIndecies[0],bQuarkJetIndecies[1]);
+		    	R = a.M()/(bQuarkJets[0]+bQuarkJets[1]).M();
+		    	bQuarkJets[0] = R*bQuarkJets[0];
+		    	bQuarkJets[1] = R*bQuarkJets[1];
+		    	H = bQuarkJets[0]+bQuarkJets[1]+a;
+		    	mHbReg.set(H.Pt(), H.Eta(), H.Phi(), H.M(), 0,1);
+		    }
+		    newTree->Fill();
+		} else {
+		    TLorentzVector m1,m2,a;
+    	    m1.SetPtEtaPhiM(rds->muPt->at(0),
+    	               rds->muEta->at(0),
+    	               rds->muPhi->at(0),0);
+   	        m2.SetPtEtaPhiM(rds->muPt->at(1),
+    	               rds->muEta->at(1),
+    	               rds->muPhi->at(1),0);                   
+   	        a = m1+m2;       
+		    //No b-flavor requested
+		    TLorentzVector bJet1,bJet2, H, ab;
+    	        bJet1.SetPtEtaPhiE(rds->jetsPt->at(0), rds->jetsEta->at(0), rds->jetsPhi->at(0), rds->jetsE->at(0));
+    	        bJet2.SetPtEtaPhiE(rds->jetsPt->at(1), rds->jetsEta->at(1), rds->jetsPhi->at(1), rds->jetsE->at(1));
+    	    ab = bJet1+bJet2;
+		    H = bJet1+bJet2+a;
+		    mH.set(H.Pt(), H.Eta(), H.Phi(), H.M(), 0,1);
+		    mHb.set(ab.Pt(), ab.Eta(), ab.Phi(), ab.M(), 0,1);
+		    double R = a.M()/(bJet1+bJet2).M();
+		    bJet1 = R*bJet1;
+		    bJet2 = R*bJet2;
+    	    ab = bJet1+bJet2;		    
+			H = bJet1+bJet2+a;
+		    mHReg.set(H.Pt(), H.Eta(), H.Phi(), H.M(), 0,1);
+		    mHbReg.set(ab.Pt(), ab.Eta(), ab.Phi(), ab.M(), 0,1);
+		    newTree->Fill();	
+		}
+	}
+    newTree->Write();
 	hamb->mkdir("CutFlowTable")->cd();
 	cutflow->Write();
 	fout->cd();
-        fout->Save();
-        fout->Close();
+    fout->Save();
+    fout->Close();
     
     	return 1;
 }
