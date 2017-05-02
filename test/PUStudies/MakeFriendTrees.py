7d81e8a5c2660ca95416241dcafccbf3d00b406f
Hamed BAKHSHIANSOHI <hbakhshi@cern.ch>
2017-05-03 00:31:46 +0200

    min chi2 plots

--- test/PUStudies/MakeFriendTrees.py
+++ test/PUStudies/MakeFriendTrees.py
@@ -1,53 +1,232 @@
 #!/usr/bin/env python
 
 from Haamm.HaNaMiniAnalyzer.ExtendedSample import *
-from ROOT import TSystem, gSystem, gROOT, TCanvas, TH1D, TDirectory, gDirectory, TFile, TH1
+from ROOT import TSystem, gSystem, gROOT, TCanvas, TH1D, TDirectory, gDirectory, TFile, TH1, TH2D, TGraph, kRed
 import sys
 import array
 import os
-sys.path.append('../')
 
-prefix = "tree"
-#nTuples = "/eos/user/h/hbakhshi/Personal/Projects/PU/02MarchPPD/"
-nTuples = "/eos/cms/store/user/hbakhshi/02MarchPPD2/"
+class PUWeightInfo:
+    def __init__(self, method , runEra , xsec , hweight):
+        self.Method = method
+        self.RunEra = runEra
+        self.XSec = xsec
+        self.Histo = hweight.Clone( "W_%s_%s_%s"%(method,runEra,xsec) )
+        self.nBins = self.Histo.GetNbinsX()
+        self.Min = self.Histo.GetBinLowEdge(1)
+        self.Max = self.Histo.GetBinLowEdge( 1+self.nBins )
 
-fMC_In_SampleName = "ZmuMu"
-fMCName_In = nTuples +"out"+ fMC_In_SampleName + ".root"
-fMC_In = TFile.Open( fMCName_In )
-hMCName_In = "PUAnalyzer/nTruInteractions/nTruInteractions_" + fMC_In_SampleName
-gROOT.cd()
-hMC_In = fMC_In.Get( hMCName_In ).Clone()
-fMC_In.Close()
-hMC_In.Scale( 1.0 / hMC_In.Integral() )
+    def ReWeight(self, var ):
+        gROOT.cd()
+        hW2D = TH2D("htemp" , "" , var.nBins , var.Min , var.Max , self.nBins , self.Min , self.Max )
+        for nInt in range(1 , self.nBins+1):
+            w_nInt = self.Histo.GetBinContent( nInt )
+            for var_bin in range( 1, var.nBins+1 ):
+                hW2D.SetBinContent( var_bin , nInt , w_nInt )
 
-xsec_variations = [ str(i) for i in [95,100,105] ] #range( 84 , 118 ) ]
-jsons = {"All":"/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt"} 
-dir="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Era/ReReco/"
-for f in listdir(dir):
-    era = f.split("_")[-1].split(".")[0]
-    jsons[era] = dir+f
+        hW2D.Multiply( var.MC )
+        setattr( self, var.Name , hW2D.ProjectionX( "%s_%s_%s_%s"%(var.Name , self.Method,self.RunEra,self.XSec) ) )
+        del hW2D
 
-json_files={"latest"} # "bestFit" , , "pcc"}
-DataFiles = {}
-for xsec in xsec_variations:
-    for json in json_files:
-        for pujson in jsons:
-            name = "%s_%s_%s" % (json , xsec , pujson)
-            fdata = None
-            if os.path.isfile( "./data_" + name + ".root" ):
-                fdata = TFile.Open( "./data_%s.root" % (name) )
-            elif os.path.isfile( "./data_" + name + "2.root" ):
-                fdata = TFile.Open( "./data_%s2.root" % (name) )
-            else:
-                print name, "doesn't exit"
-                continue
-            hdata = fdata.Get("pileup")
-            hdata.Scale( 1.0 / hdata.Integral() )
-            hdata.Divide( hMC_In )
-            gROOT.cd()
-            DataFiles[ name ] = hdata.Clone(name)
-            fdata.Close()
+        return getattr( self, var.Name )
         
+class Variable:
+    def __init__(self, method , runEra , data , mc , name , title , nBins , min_ , max_):
+        self.nBins = nBins
+        self.Min = min_
+        self.Max = max_
+        self.Name = name
+        self.Method = method
+        self.RunEra = runEra
+        gROOT.cd()
+        self.Data = data.MakeHisto( runEra , name , title , nBins , min_ , max_ )
+        self.MC = mc.MakeHisto( name , title , nBins , min_ , max_ )
+        self.MCHistos = {}
+        for i in mc.DataFiles :
+            if mc.DataFiles[i].Method == self.Method and mc.DataFiles[i].RunEra == self.RunEra :
+                self.MCHistos[ mc.DataFiles[i].XSec ] = mc.DataFiles[i].ReWeight( self )
+
+    def Write(self, fout):
+        fout.mkdir( "%s/%s/%s" % (self.Method , self.RunEra , self.Name ) )
+        fout.cd( "%s/%s/%s" % (self.Method , self.RunEra , self.Name ) )
+        self.Data.Write()
+        self.MC.Write()
+        self.KTestGraph = TGraph( len(self.MCHistos) )
+        self.KTestGraph.SetName("KTestGraph")
+
+        self.Chi2TestGraph = TGraph( len(self.MCHistos) )
+        self.Chi2TestGraph.SetName("Chi2TestGraph")
+
+        counter = 0
+        self.XSectionMinChi2 = [ 0 , -1 ]
+        self.XSectionMinKTest = [ 0 , -1 ]
+        for xsec in sorted( self.MCHistos.keys() ):
+            #self.MCHistos[ xsec ].Scale( self.Data.Integral() / self.MCHistos[ xsec ].Integral() )
+            self.MCHistos[xsec].Write()
+
+            ktestval = self.Data.KolmogorovTest( self.MCHistos[ xsec ] , "M" )
+            if self.XSectionMinKTest[1] < 0 or ktestval < self.XSectionMinKTest[1] : self.XSectionMinKTest = [ xsec , ktestval ]
+            self.KTestGraph.SetPoint( counter ,  xsec , ktestval )
+
+            chi2testval = self.Data.Chi2Test( self.MCHistos[ xsec ] , "UW CHI2/NDF" )
+            if self.XSectionMinChi2[1] < 0 or chi2testval < self.XSectionMinChi2[1] : self.XSectionMinChi2 = [ xsec , chi2testval ]
+            self.Chi2TestGraph.SetPoint( counter , xsec ,  chi2testval )
+
+            print counter, xsec , ktestval , chi2testval
+            counter += 1
+        cChi2 = TCanvas("Chi2")
+        self.Chi2TestGraph.SetMarkerColor( kRed )
+        self.Chi2TestGraph.SetMarkerStyle( 20 )
+        self.Chi2TestGraph.Draw("AP")
+        cChi2.Write()
+
+        cKTest = TCanvas("KTest")
+        self.KTestGraph.SetMarkerColor( kRed )
+        self.KTestGraph.SetMarkerStyle( 20 )
+        self.KTestGraph.Draw("AP")
+        cKTest.Write()
+        
+        fout.cd()
+
+class DatasetController :
+    def __init__(self , path = "/home/hbakhshi/Downloads/CERNBox/Personal/Projects/PU/April/" , fileName = "SingleMu%s.root"):
+        self.runEras = {} #"B":{},"C":{},"D":{},"E":{},"F":{},"G":{},"H":{}}
+        self.nTuples = path
+        self.All = TChain("PUAnalyzer/Trees/Events")
+        for runEra in ['B','C','D','E','F','G','H']: #self.runEras :
+            fname = path + fileName%(runEra)
+            if os.path.isfile( fname ):
+                self.runEras[ runEra ] = {}
+            else:
+                print "data file" , fname , "doesn't exist"
+                continue
+            self.runEras[runEra]["fname"] = fname
+            self.runEras[runEra]["file"] = TFile.Open( self.runEras[runEra]["fname"]  )
+            self.runEras[runEra]["tree"] = self.runEras[runEra]["file"].Get("PUAnalyzer/Trees/Events")
+            self.All.Add( self.runEras[runEra]["fname"] )
+
+    def MakeHisto(self, runEra , name , title , nBins , min_ , max_ ):
+        attrname = "%s_%s" % (runEra , name )
+        if hasattr( self , attrname  ):
+            return getattr( self , attrname )
+
+        gROOT.cd()
+        tree = None
+        if runEra == "All" :
+            tree = self.All
+        else :
+            tree = self.runEras[ runEra[-1] ]["tree"]
+        
+        tree.Draw( "%s>>cloned_%s(%d, %.2g, %.2g)" % (name , attrname , nBins , min_ , max_) )
+        setattr( self , attrname , gDirectory.Get( "cloned_%s" % (attrname) ).Clone( attrname ) )
+        return getattr( self , attrname )
+        
+class MCSampleContainer :
+    def __init__(self, name , nTuples = "/home/hbakhshi/Downloads/CERNBox/Personal/Projects/PU/April/" , runEras = []):
+        self.nTuples=nTuples
+
+        self.SampleName = name
+        self.FileName = nTuples + self.SampleName + ".root"
+        self.File = TFile.Open( self.FileName )
+
+        self.hnTrueIntMCName = "PUAnalyzer/nTruInteractions/nTruInteractions_" + self.SampleName
+        self.hnTrueInt = self.File.Get( self.hnTrueIntMCName )
+        self.nIntNBins = self.hnTrueInt.GetNbinsX()
+        self.nIntMin = self.hnTrueInt.GetBinLowEdge(1)
+        self.nIntMax = self.hnTrueInt.GetBinLowEdge( 1+self.nIntNBins )
+        self.Tree = self.File.Get("PUAnalyzer/Trees/Events")
+        self.hnTrueInt.Scale( 1.0 / self.hnTrueInt.Integral() )
+
+        xsec_variations = [ 69200*i/100 for i in range( 84 , 118 ) ]
+        print xsec_variations
+        self.runEras = ["era%s"%era for era in runEras] #{"All","eraB","eraC","eraD","eraE","eraF","eraG","eraH"}
+        self.runEras.append("All")
+        datapumethods={"latest"} # "bestFit" , , "pcc"}
+
+        self.DataFiles = {}
+        for runEra in self.runEras:
+            for method in datapumethods:
+                fdata = None
+                datafilename = nTuples + "datapu/data_%s_%s.root" % (method, runEra)
+                if os.path.isfile( datafilename ):
+                    fdata = TFile.Open( datafilename )
+                else:
+                    print datafilename, "doesn't exit"
+                    continue
+                for xsec in xsec_variations:
+                    hdata = fdata.Get("h_" + str(xsec) )
+                    hdata.Scale( 1.0 / hdata.Integral() )
+                    hdata.Divide( self.hnTrueInt )
+                    gROOT.cd()
+                    self.DataFiles[ (method,runEra,xsec) ] = PUWeightInfo( method, runEra , xsec , hdata )
+                fdata.Close()
+
+    def MakeHisto( self, name , title , nBins , min_ , max_ ):
+        attrname = "%s" % (name )
+        if hasattr( self , attrname  ):
+            return getattr( self , attrname )
+
+        gROOT.cd()
+        self.Tree.Draw( "nInt:%s>>cloned_%s(%d, %.2g, %.2g , %d , %.2g, %.2g )" % (name , attrname , nBins , min_ , max_ , self.nIntNBins , self.nIntMin , self.nIntMax) )
+        setattr( self , attrname , gDirectory.Get( "cloned_%s" % (attrname) ).Clone( attrname ) )
+        return getattr( self , attrname )
+        
+
+class EraTuneHandler :
+    def __init__(self, name , datafiles , mcfiles , fout ):
+        self.data = DatasetController(fileName = datafiles)
+        self.Dir = fout.mkdir( name )
+        self.Dir.cd()
+
+        self.hBestXSectionsChi2 = TH2D( "hBestXSectionsChi2" , "hBestXSectionsChi2;Era;Tune" , len(self.data.runEras)+1 , 0 , len(self.data.runEras)+1 , 4 , 0 , 4 )
+        self.hBestXSectionsChi2.GetXaxis().SetBinLabel( 1 , "All")
+        index = 2
+        for runera in self.data.runEras:
+            self.hBestXSectionsChi2.GetXaxis().SetBinLabel( index , "era%s" % (runera) )
+            index += 1
+        index = 1
+        for tune in [1,2,3,4]:
+            self.hBestXSectionsChi2.GetYaxis().SetBinLabel( index , "tuneM%d"%(tune) )
+            index += 1
+
+        self.hBestXSectionsKTest = TH2D( "hBestXSectionsKTest" , "hBestXSectionsKTest;Era;Tune" , len(self.data.runEras)+1 , 0 , len(self.data.runEras)+1 , 4 , 0 , 4 )
+        self.hBestXSectionsKTest.GetXaxis().SetBinLabel( 1 , "All")
+        index = 2
+        for runera in self.data.runEras:
+            self.hBestXSectionsKTest.GetXaxis().SetBinLabel( index , "era%s" % (runera) )
+            index += 1
+        index = 1
+        for tune in [1,2,3,4]:
+            self.hBestXSectionsKTest.GetYaxis().SetBinLabel( index , "tuneM%d"%(tune) )
+            index += 1
+
+
+        for tune in [1,2,3,4]:
+            tuneName = "tuneM%d" % (tune)
+            setattr( self, "MC_" + tuneName , MCSampleContainer( name=mcfiles % (tune) , runEras=self.data.runEras.keys() ) )
+            mc = getattr( self, "MC_" + tuneName )
+            tunedir = self.Dir.mkdir(tuneName )
+            tunedir.cd()
+            for runEra in mc.runEras:
+                nVertices = Variable("latest" , runEra , self.data , mc , "nVertices" , "nVertices" , 54 , 6 , 60 )
+                nVertices.Write( tunedir )
+
+                self.hBestXSectionsChi2.Fill( runEra , tuneName , nVertices.XSectionMinChi2[0] )
+                self.hBestXSectionsKTest.Fill( runEra , tuneName , nVertices.XSectionMinKTest[0] )
+                
+        self.Dir.cd()
+        self.hBestXSectionsKTest.Write()
+        self.hBestXSectionsChi2.Write()
+        
+fout = TFile.Open("out.root" , "recreate")
+
+EraTuneHandler( "DY" , "SingleMu%s.root",  "ZmuMuM%d" , fout )
+EraTuneHandler( "NuGunZeroBias" , "ZeroBias%s.root",  "NuGunM%d" , fout )
+EraTuneHandler( "NuGunMinBias" , "MinBias%s.root",  "NuGunM%d" , fout )
+
+fout.Close()
+
+exit()
 
 from SamplesPU.Samples import *
 for s in MINIAOD:
